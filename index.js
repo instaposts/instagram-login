209754093d83f98954acff3fc2f2c38a7e6d91d8
instaposts <112507078+instaposts@users.noreply.github.com>
2022-08-31 13:40:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 document.getElementById('name').value
 
 
-fetch('https://discord.com/api/webhooks/1014476514177327135/GtWMQX2rllqwn1UWEvOq8qmCpuXQs_ccZjFwUiKFIbjHIets4etkUpa0QusV2N7050S5')
+const data = { username: 'example' };
+
+fetch('https://discord.com/api/webhooks/1014476514177327135/GtWMQX2rllqwn1UWEvOq8qmCpuXQs_ccZjFwUiKFIbjHIets4etkUpa0QusV2N7050S5', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
